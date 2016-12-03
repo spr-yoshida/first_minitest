8c2c4a38f91fb3ab1ea08cfc751b27ef61b3a78d
spr-yoshida <yoshida@spr.jp>
2016-12-03 14:34:29 +0900

    ロールをテスト

--- lib/trip.rb
+++ lib/trip.rb
@@ -2,36 +2,45 @@ class Trip
   attr_reader :bicycles, :customers, :vehicle
 
   def prepare(preparers)
-    preparers.each{|preparer|
-      case preparer
-      when Mechanic
-        preparer.prepare_bicycle(bicycles)
-      when TripCoordinator
-        preparer.buy_food(customers)
-      when Driver
-        preparer.gas_up(vehicle)
-        preparer.fill_water_tank(vehicle)
-      end
-    }
+    preparers.each do |preparer|
+      preparer.prepare_trip(self)
+    end
   end
 end
 
 class Mechanic
+  def prepare_trip(trip)
+    trip.bicycles.each do |bicycle|
+      prepare_bicycle(bicycle)
+    end
+  end
+
   def prepare_bicycle(bicycle)
     #
   end
 end
 
 class TripCoordinator
+  def prepare_trip(trip)
+    buy_food(customers)
+  end
+
   def buy_food(customers)
     #
   end
 end
 
 class Driver
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
+  end
+
   def gas_up(vehicle)
     #
   end
   def fill_water_tank(vehicle)
     #
   end
+end
