700721eb37a65cd35a9bd8b30fd75c3d54df5d7a
spr-yoshida <yoshida@spr.jp>
2016-12-03 13:49:12 +0900

    ロールとして依存オブジェクトを注入する

--- test/test_parts.rb
+++ test/test_parts.rb
@@ -5,10 +5,27 @@ require_relative 'test_helper'
 require 'parts'
 
 class WheelTest < MiniTest::Unit::TestCase
+  def setup
+    @wheel = Wheel.new(26,1.5)
+  end
+
+  def test_implements_the_diameterizable_interface
+    assert_respond_to(@wheel,:diameter)
+  end
+
   def test_calculates_diameter
     wheel = Wheel.new(26,1.5)
 
-    assert_in_delta(29,wheel.diameter,0.01)
+    assert_in_delta(
+      29,
+      wheel.diameter,
+      0.01)
+  end
+end
+
+class DiameterDouble
+  def diameter
+    10
   end
 end
 
@@ -17,10 +34,10 @@ class GearTest < MiniTest::Unit::TestCase
     gear = Gear.new(
       chainring:52,
       cog:11,
-      wheel:Wheel.new(26,1.5))
+      wheel:DiameterDouble.new)
 
     assert_in_delta(
-      137.1,
+      47.27,
       gear.gear_inches,
       0.01)
   end
