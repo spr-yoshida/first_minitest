778b2765a92da607a1358620f405716768b6a137
spr-yoshida <yoshida@spr.jp>
2016-12-03 14:06:02 +0900

    コマンドメッセージのテスト

--- lib/parts.rb
+++ lib/parts.rb
@@ -5,17 +5,32 @@ class Wheel
     @tire = tire
   end
 
-  def width
+  def diameter
     rim + (tire * 2)
   end
 end
 
 class Gear
-  attr_reader :chainring, :cog, :wheel
+  attr_reader :chainring, :cog, :wheel, :observer
   def initialize(args)
     @chainring = args[:chainring]
     @cog = args[:cog]
     @wheel = args[:wheel]
+    @observer = args[:observer]
+  end
+
+  def set_cog(new_cog)
+    @cog = new_cog
+    changed
+  end
+
+  def set_chainring(new_chainring)
+    @chainring = new_chainring
+    changed
+  end
+
+  def changed
+    observer.changed(chainring,cog)
   end
 
   def gear_inches
--- test/test_parts.rb
+++ test/test_parts.rb
@@ -30,6 +30,26 @@ class DiameterDouble
 end
 
 class GearTest < MiniTest::Unit::TestCase
+  def setup
+    @observer = MiniTest::Mock.new
+    @gear = Gear.new(
+      chainring:52,
+      cog:11,
+      observer:@observer)
+  end
+
+  def test_notifies_observers_when_cogs_change
+    @observer.expect(:changed,true,[52,27])
+    @gear.set_cog(27)
+    @observer.verify
+  end
+
+  def test_notifies_observers_when_chainrings_change
+    @observer.expect(:changed,true,[42,11])
+    @gear.set_chainring(42)
+    @observer.verify
+  end
+
   def test_calculates_grar_inches
     gear = Gear.new(
       chainring:52,
